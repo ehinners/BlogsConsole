6eaae2b7c3ebe799ee3f23e7fa1bb877a7079c6f
ehinners <ehinners@my.wctc.edu>
2021-10-30 16:50:14 -0500

    Base MVC Objects created

---
+++ Controller.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.IO;
+using System.Linq;
+using NLog.Web;
+
+namespace BlogsConsole
+{  
+    public static class Controller
+    {
+    }
+}
---
+++ View.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.IO;
+using System.Linq;
+using NLog.Web;
+
+namespace BlogsConsole
+{  
+    public static class View
+    {
+        
+
+    }
+}
